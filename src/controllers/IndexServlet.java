5d40a9e0478f0697c9df28bad0d75450cc439756
Yuu.Fujimoto <yuusantaro@gmaik.com>
2020-12-20 19:11:27 +0900

    Add NewServlet.java

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -1,12 +1,18 @@
 package controllers;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.Tasks;
+import utils.DBUtil;
+
 
 /**
  * Servlet implementation class IndexServlet
@@ -27,8 +33,12 @@ public class IndexServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        response.getWriter().append("Served at: ").append(request.getContextPath());
+        EntityManager em = DBUtil.createEntityManager();
+
+        List<Tasks> tasks = em.createNamedQuery("getAllTasks", Tasks.class).getResultList();
+        response.getWriter().append(Integer.valueOf(tasks.size()).toString());
+
+        em.close();
     }
 
 }
--- src/models/tasks.java
+++ src/models/tasks.java
@@ -1,3 +1,5 @@
+package models;
+
 import java.sql.Timestamp;
 
 import javax.persistence.Column;
@@ -5,9 +7,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
+@NamedQueries({
+    @NamedQuery(
+        name = "getAllTasks",
+        query = "SELECT m FROM Tasks AS m ORDER BY m.id DESC"
+    )
+})
 @Table(name = "tasks")
 public class Tasks {
     @Id
@@ -15,14 +25,14 @@ public class Tasks {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(name = "content", length = 255, nullable = false)
+    private String content;
+
     @Column(name = "created_at", nullable = false)
     private Timestamp created_at;
 
     @Column(name = "updated_at", nullable = false)
     private Timestamp updated_at;
-
-    @Column(name = "content", length = 255, nullable = false)
-    private String content;
 
     public Integer getId() {
         return id;
@@ -31,6 +41,7 @@ public class Tasks {
     public void setId(Integer id) {
         this.id = id;
     }
+
 
     public String getContent() {
         return content;
