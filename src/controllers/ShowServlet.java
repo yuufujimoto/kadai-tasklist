f0026bf4fd0a58248383d15e826132372a1316c0
Yuu.Fujimoto <yuusantaro@gmaik.com>
2020-12-20 20:18:13 +0900

    Add ShowServlet

--- src/controllers/ShowServlet.java
+++ src/controllers/ShowServlet.java
@@ -38,7 +38,7 @@ public class ShowServlet extends HttpServlet {
 
         em.close();
 
-        // タスクデータをリクエストスコープにセットしてshow.jspを呼び出す
+        // メッセージデータをリクエストスコープにセットしてshow.jspを呼び出す
         request.setAttribute("task", m);
 
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/show.jsp");
